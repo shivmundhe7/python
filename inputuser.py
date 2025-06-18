d1f08adad3e679032c264187e5564447a2002da0
shivmundhe7 <shivmundhe16@gmail.com>
2025-06-18 17:14:37 +0530

    Python

--- inputuser.py
+++ inputuser.py
@@ -1,7 +1,11 @@
-# Name of the user
-a = input("Enter your name :")
-print("My name is " , a)
+# # Name of the user
+# a = input("Enter your name :")
+# print("My name is " , a)
 
-x = input("Enter first number :")
-y = input("Enter second number :")
-print(int(x) + int(y))
+# x = input("Enter first number :")
+# y = input("Enter second number :")
+# print(int(x) + int(y))
+
+x = input("firstname")
+y = input("secondname")
+print("Hello", x,y,"!","You just delved into python.")
---
+++ strings.py
@@ -0,0 +1,13 @@
+name = "Shiv"
+friend = "Raju"
+brother = "Sangam"
+# ''' Three Commas Will be used for strings are in diffrent line instead of (\n) we can use (''') commas also 
+apple = '''he said,
+hi Shiv
+hey i am good
+'i want to eat an apple'''
+print(apple) 
+print("My Name is ", name , "My Friend name is ",friend, "and","My brother name is ",brother)
+
+print(name[0])
+print(name[1])
