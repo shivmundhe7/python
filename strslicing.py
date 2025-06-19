420d839cd12a8298d315e1737561c33b2d0a286c
shivmundhe7 <shivmundhe16@gmail.com>
2025-06-19 12:29:37 +0530

    Python

---
+++ strslicing.py
@@ -0,0 +1,17 @@
+names = "Shiv,sangam"
+
+# Length of a string
+print(len(names))
+
+fruit = "mango"
+len1 = len(fruit)
+print("The mango is a ", len1 , "Letter word.")
+print(fruit[0:3])
+
+# loops through string
+alphabets = "ABCTE"
+for i in alphabets: 
+        print(i)
+
+nm = "shiv"
+print(nm[-4:-2])
