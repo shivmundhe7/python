074e7af1e3524039db82143eddae461f53dbfc73
shivmundhe7 <shivmundhe16@gmail.com>
2025-06-22 00:01:15 +0530

    Python

---
+++ docstrings.py
@@ -0,0 +1,8 @@
+def square(n):
+    '''Takes in a number n, returns the squre of n'''
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+# PEP 8
+# THAT PROVIDES GUIDELINES AND BEST PRACTICES ON HOW TO WRITE TO PYTHON CODE
---
+++ f-strings.py
@@ -0,0 +1,6 @@
+letter = "Hey my name is {} and i am from {}"
+country = "India"
+name = "Shiv"
+
+print(letter.format(name , country))
+print(f"hey my name is {name} and i am from {country}")
